eda2e7eb092a169ca4387dc1e860bfee24aa2448
unknown <ishanmadhawa09@gmail.com>
2022-09-13 22:47:53 +0530

    class finished

--- class.js
+++ class.js
@@ -1,8 +1,21 @@
 "use strict";
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _Employee_id;
 class Employee {
     //typescript not supported for multiple constructers
     constructor(id, name, address) {
-        this.id = id;
+        _Employee_id.set(this, void 0);
+        __classPrivateFieldSet(this, _Employee_id, id, "f");
         this.name = name;
         this.address = address;
     }
@@ -10,10 +23,39 @@ class Employee {
     getNameWithAddress() {
         return `${this.name} Stays at ${this.address}`;
     }
+    //static methods it depond on class not object instance
+    static getEmployeeCount() {
+        return 50;
+    }
+    //getters and setters
+    get EmpId() {
+        return __classPrivateFieldGet(this, _Employee_id, "f");
+    }
+    set EmpId(value) {
+        __classPrivateFieldSet(this, _Employee_id, value, "f");
+    }
 }
+_Employee_id = new WeakMap();
 let ishan = new Employee(1, "ishan", "Colmbo SL");
+console.log(ishan);
+ishan.EmpId = 100;
+console.log(ishan.EmpId);
 let address = ishan.getNameWithAddress();
+//calling without object instance
+Employee.getEmployeeCount();
 // ishan.id = 1;
 // ishan.name = "Ishan";
 // ishan.address = "colombo sl";
 console.log(address);
+//inhertance 
+class Manager extends Employee {
+    constructor(id, name, address) {
+        super(id, name, address);
+    }
+    // method over riding
+    getNameWithAddress() {
+        return `${this.name} Manager Stays at ${this.address}`;
+    }
+}
+let kavindya = new Manager(2, "Kavindya", "Rathanapura");
+console.log(kavindya.getNameWithAddress());
--- class.ts
+++ class.ts
@@ -1,11 +1,11 @@
 class Employee{
-    id:number;
+    #id:number;
     name:string;
-    address:string;
+    protected address:string;
 
     //typescript not supported for multiple constructers
     constructor(id:number, name:string, address:string){
-        this.id = id;
+        this.#id = id;
         this.name = name;
         this.address = address;
     }
@@ -13,11 +13,43 @@ class Employee{
     getNameWithAddress():string{
         return `${this.name} Stays at ${this.address}`;
     } 
+    //static methods it depond on class not object instance
+    static getEmployeeCount():number{
+        return 50;
+    }
+
+    //getters and setters
+    get EmpId():number{
+        return this.#id;
+    }
+
+    set EmpId(value:number){
+        this.#id=value;
+    }
 }
 
 let ishan = new Employee(1,"ishan","Colmbo SL");
+console.log(ishan);
+ishan.EmpId = 100;
+console.log(ishan.EmpId);
 let address = ishan.getNameWithAddress();
+//calling without object instance
+Employee.getEmployeeCount();
 // ishan.id = 1;
 // ishan.name = "Ishan";
 // ishan.address = "colombo sl";
 console.log(address);
+
+//inhertance 
+class Manager extends Employee{
+    constructor(id:number, name:string, address:string){
+        super(id,name,address);
+    }
+    // method over riding
+    getNameWithAddress():string{
+        return `${this.name} Manager Stays at ${this.address}`;
+    } 
+}
+
+let kavindya = new Manager(2,"Kavindya","Rathanapura");
+console.log(kavindya.getNameWithAddress());
