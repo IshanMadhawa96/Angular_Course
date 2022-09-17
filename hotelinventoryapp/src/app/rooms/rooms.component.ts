37f10efa7bdee8a9961fb939667599b9766ca2f7
unknown <ishanmadhawa09@gmail.com>
2022-09-17 21:59:04 +0530

    Interface added

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from './rooms';
 
 @Component({
   selector: 'hniv-rooms',
@@ -10,6 +11,11 @@ export class RoomsComponent implements OnInit {
   hotelName = 'Ish Hotel';
   numberOfRooms = 10;
   hideRooms = false;
+  rooms:Room={
+    totalRoom:100,
+    availableRooms:10,
+    bookedRooms:10
+  };
   constructor() { }
 
   ngOnInit(): void {
@@ -20,3 +26,19 @@ export class RoomsComponent implements OnInit {
   }
 
 }
+/*
+Property binding in Angular helps you
+set values for properties of HTML elements or directives.
+Use property binding to do things such as toggle button functionality,
+set paths programmatically, and share values between components.
+*/
+
+/**
+ * Event binding lets you listen for and respond to
+  user actions such as keystrokes, mouse movements, clicks, and touches.
+ */
+
+  /*
+  Interpolation refers to embedding expressions into marked up text.
+  By default, interpolation uses the double curly braces {{ and }} as delimiters.
+  */
---
+++ hotelinventoryapp/src/app/rooms/rooms.ts
@@ -0,0 +1,5 @@
+export interface Room{
+  totalRoom:number;
+  availableRooms:number;
+  bookedRooms:number;
+}
