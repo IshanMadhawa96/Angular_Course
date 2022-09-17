55a08ceadb53b1f9cab77cf377ff7926d8f0bf7d
unknown <ishanmadhawa09@gmail.com>
2022-09-17 18:37:58 +0530

    hello world

--- hotelinventoryapp/src/app/app.component.ts
+++ hotelinventoryapp/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:'Hello world from Inline Template',
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
