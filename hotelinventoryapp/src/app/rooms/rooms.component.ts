685422ce3657ca0121239c9ce45349c0ba629bd6
unknown <ishanmadhawa09@gmail.com>
2022-10-13 23:55:59 +0530

    room list component added

--- hotelinventoryapp/src/app/rooms/rooms.component.ts
+++ hotelinventoryapp/src/app/rooms/rooms.component.ts
@@ -17,42 +17,46 @@ export class RoomsComponent implements OnInit {
     bookedRooms:10
   };
 
-  roomList :RoomList[] =[
-    {
-    roomNumber:1,
-    roomType : 'Deluxe Room',
-    amenities: 'AC,Free wifi,TV,Bathroom',
-    price:500,
-    photo  : "",
-    checkingTime : new Date ('11-nov-2020'),
-    checkingoutTime: new Date ('11-nov-2020'),
-    rating:4.54645646
-  },
-  {
-    roomNumber:2,
-    roomType : 'Deluxe Room',
-    amenities: 'AC,Free wifi,TV,Bathroom',
-    price:1500,
-    photo  : "",
-    checkingTime : new Date ('11-nov-2020'),
-    checkingoutTime: new Date ('11-nov-2020'),
-    rating:3.54564645
-  },
-  {
-    roomNumber:3,
-    roomType : 'Private Room',
-    amenities: 'AC,Free wifi,TV,Bathroom',
-    price:1500,
-    photo  : "",
-    checkingTime : new Date ('11-nov-2020'),
-    checkingoutTime: new Date ('11-nov-2020'),
-    rating:3.5456456
-  },
+  roomList:RoomList[]=[];
 
-]
+
   constructor() { }
 
   ngOnInit(): void {
+    this. roomList  =[
+      {
+      roomNumber:1,
+      roomType : 'Deluxe Room',
+      amenities: 'AC,Free wifi,TV,Bathroom',
+      price:500,
+      photo  : "",
+      checkingTime : new Date ('11-nov-2020'),
+      checkingoutTime: new Date ('11-nov-2020'),
+      rating:4.54645646
+    },
+    {
+      roomNumber:2,
+      roomType : 'Deluxe Room',
+      amenities: 'AC,Free wifi,TV,Bathroom',
+      price:1500,
+      photo  : "",
+      checkingTime : new Date ('11-nov-2020'),
+      checkingoutTime: new Date ('11-nov-2020'),
+      rating:3.54564645
+    },
+    {
+      roomNumber:3,
+      roomType : 'Private Room',
+      amenities: 'AC,Free wifi,TV,Bathroom',
+      price:1500,
+      photo  : "",
+      checkingTime : new Date ('11-nov-2020'),
+      checkingoutTime: new Date ('11-nov-2020'),
+      rating:3.5456456
+    },
+
+  ]
+  
   }
 
   toggle(){
@@ -115,3 +119,20 @@ set paths programmatically, and share values between components.
 
 
 
+/*
+
+Lifecycle hooks
+A component instance has a lifecycle that starts when Angular instantiates the component class
+and renders the component view along with its child views. The lifecycle continues with change detection,
+as Angular checks to see when data-bound properties change,
+and updates both the view and the component instance as needed.
+The lifecycle ends when Angular destroys the component instance and
+removes its rendered template from the DOM.
+Directives have a similar lifecycle, as Angular creates,
+updates, and destroys instances in the course of execution.
+
+Your application can use lifecycle hook methods to tap into key events in the
+lifecycle of a component or directive to initialize new instances,
+initiate change detection when needed, respond to updates during change detection,
+and clean up before deletion of instances.
+*/
